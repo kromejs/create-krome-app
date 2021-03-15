09cc83e97d1efb384bfc3f16c2bc21acd697ff6a
hank <hankchiu.tw@gmail.com>
2021-03-16 07:56:24 +0800

    chore: update eslintrc

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,27 @@
-import {Command, flags} from '@oclif/command'
+import { Command, flags } from '@oclif/command';
 
 class CreateKromeApp extends Command {
   static description = 'Generate the krome starter app';
 
   static flags = {
     // add --version flag to show CLI version
-    version: flags.version({char: 'v'}),
-    help: flags.help({char: 'h'}),
+    version: flags.version({ char: 'v' }),
+    help: flags.help({ char: 'h' }),
     // flag with a value (-n, --name=VALUE)
-    name: flags.string({char: 'n', description: 'name to print'}),
+    name: flags.string({ char: 'n', description: 'name to print' }),
     // flag with no value (-f, --force)
-    force: flags.boolean({char: 'f'}),
+    force: flags.boolean({ char: 'f' }),
   };
 
-  static args = [{name: 'appName', required: true, description: 'the app name'}];
+  static args = [{ name: 'appName', required: true, description: 'the app name' }];
 
   async run() {
-    const {args, flags} = this.parse(CreateKromeApp)
+    const { args, flags } = this.parse(CreateKromeApp);
 
-    const name = flags.name ?? 'world'
-    this.log(`hello ${name} from ./src/index.ts`)
+    const name = flags.name ?? 'world';
+    this.log(`hello ${name} from ./src/index.ts`);
     if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
+      this.log(`you input --force and --file: ${args.file}`);
     }
   }
 }
