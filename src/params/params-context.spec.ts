07545c9d8e569c8c4023e3196024ac1a205143da
hank <hankchiu.tw@gmail.com>
2021-04-24 22:22:53 +0800

    feat: enable mocha

---
+++ src/params/params-context.spec.ts
@@ -0,0 +1,8 @@
+import { expect } from 'chai';
+import { ParamsContext } from './params-context';
+
+describe('ParamsContext', () => {
+  it('should create', () => {
+    expect(new ParamsContext()).to.be.ok;
+  });
+});
